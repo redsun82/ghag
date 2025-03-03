795d27df621b8be063604edb3cec4b61cdcf3f71
Paolo Tranquilli <redsun82@gmail.com>
2025-03-03 06:45:08 +0100

    Add --learn-new

--- conftest.py
+++ conftest.py
@@ -10,12 +10,15 @@ def pytest_addoption(parser):
     parser.addoption(
         "--learn", action="store_true", help="learn test output"
     )
+    parser.addoption(
+        "--learn-new", action="store_true", help="learn new test output"
+    )
 
-def check_generation(f, learn=False):
+def check_generation(f, learn=False, learn_new=False):
     wf = workflow(f)
     output = generate(wf, pathlib.Path(inspect.getfile(f)).parent)
     expected_file = output.with_suffix(".expected.yml")
-    if learn:
+    if learn or (learn_new and not expected_file.exists()):
         output.rename(expected_file)
         return
     with open(output) as out:
@@ -28,5 +31,5 @@ def check_generation(f, learn=False):
 
 def generation_test(f):
     def wrapper(pytestconfig):
-        check_generation(f, pytestconfig.getoption("--learn"))
+        check_generation(f, pytestconfig.getoption("--learn"), pytestconfig.getoption("--learn-new"))
     return wrapper
