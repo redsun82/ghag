e348d5d0e7f011fe398a297be78a3bc13cbd6861
Paolo Tranquilli <redsun82@github.com>
2025-03-05 12:10:18 +0100

    Add all expr ops

--- src/pyactions/expr.py
+++ src/pyactions/expr.py
@@ -1,6 +1,7 @@
 import enum
 import dataclasses
 import typing
+from importlib.resources import open_binary
 from typing import Self, Any
 
 from . import element
@@ -8,16 +9,27 @@ from . import element
 __all__ = ["Expr"]
 
 
-class _Op(enum.Enum):
-    none = None
-    conjunction = "&&"
-    disjunction = "||"
+_ops = [
+    None,
+    "[]",
+    ".",
+    "!",
+    " < ",
+    " <= ",
+    " > ",
+    " >= ",
+    " == ",
+    " != ",
+    " && ",
+    " || ",
+]
+_ops = {op: i for i, op in enumerate(_ops)}
 
 
 @dataclasses.dataclass
 class Expr(element.Element):
     _value: str
-    _op: str | None = None
+    _op_index: int = 0
 
     def asdict(self) -> typing.Any:
         return str(self)
@@ -25,14 +37,16 @@ class Expr(element.Element):
     def __str__(self):
         return f"${{{{ {self._value} }}}}"
 
-    def _as_and_operand(self) -> str:
-        return f"({self._value})" if self._op == "||" else self._value
+    def _as_operand(self, op_index: int) -> str:
+        if self._op_index > op_index:
+            return f"({self._value})"
+        return self._value
 
     @classmethod
-    def _syntax(cls, v: Any, within_and: bool = False) -> str:
+    def _syntax(cls, v: Any, op_index: int | None = None) -> str:
         match v:
-            case Expr() as e if within_and:
-                return e._as_and_operand()
+            case Expr() as e if op_index is not None:
+                return e._as_operand(op_index)
             case Expr() as e:
                 return e._value
             case str() as s:
@@ -40,21 +54,61 @@ class Expr(element.Element):
             case _:
                 return str(v)
 
+    @classmethod
+    def _binop(cls, lhs: Any, rhs: Any, op: str) -> Self:
+        op_index = _ops[op]
+        return Expr(
+            f"{cls._syntax(lhs, op_index)}{op}{cls._syntax(rhs, op_index)}", op_index
+        )
+
     def __and__(self, other: Any) -> Self:
-        return Expr(f"{self._as_and_operand()} && {self._syntax(other, True)}", "&&")
+        return self._binop(self, other, " && ")
 
     def __rand__(self, other: Any) -> Self:
-        return Expr(f"{self._syntax(other, True)} && {self._as_and_operand()}", "&&")
+        return self._binop(other, self, " && ")
 
     def __or__(self, other: Any) -> Self:
-        return Expr(f"{self._value} || {self._syntax(other)}", "||")
+        return self._binop(self, other, " || ")
 
     def __ror__(self, other: Any) -> Self:
-        return Expr(f"{self._syntax(other)} || {self._value}", "||")
+        return self._binop(other, self, " || ")
 
     def __invert__(self) -> Self:
-        operand = f"({self._value})" if self._op else self._value
-        return Expr(f"!{operand}")
+        op_index = _ops["!"]
+        return Expr(f"!{self._as_operand(op_index)}", op_index)
+
+    def __eq__(self, other: Any) -> Self:
+        return self._binop(self, other, " == ")
+
+    def __ne__(self, other: Any) -> Self:
+        return self._binop(self, other, " != ")
+
+    def __le__(self, other):
+        return self._binop(self, other, " <= ")
+
+    def __lt__(self, other):
+        return self._binop(self, other, " < ")
+
+    def __ge__(self, other):
+        return self._binop(self, other, " >= ")
+
+    def __gt__(self, other):
+        return self._binop(self, other, " > ")
+
+    def __getitem__(self, key: Any):
+        op_index = _ops["[]"]
+        return Expr(
+            f"{self._as_operand(op_index)}[{self._syntax(key, op_index)}]", op_index
+        )
+
+    def __getattr__(self, key: str):
+        op_index = _ops["."]
+        return Expr(f"{self._as_operand(op_index)}.{key}")
+
+    def __bool__(self):
+        raise TypeError(
+            "Expr cannot be coerced to bool: did you mean to use `&` for `and` or `|` for `or`?"
+        )
 
 
 type Value[T] = Expr | T
---
+++ tests/test_expr.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.pyactions.expr import *
+
+
+def test_ops():
+    a = Expr("a")
+    b = Expr("b")
+    c = Expr("c")
+
+    assert str(a | (b & c)) == "${{ a || b && c }}"
+    assert str((a | b) & c) == "${{ (a || b) && c }}"
+    assert str(~a | b) == "${{ !a || b }}"
+    assert str(~(a | b)) == "${{ !(a || b) }}"
+    assert str((a & b)[0]) == "${{ (a && b)[0] }}"
+    assert str((a & b).x) == "${{ (a && b).x }}"
+    assert str((a == b) | (a < c)) == "${{ a == b || a < c }}"
+    assert str((0 != a) & (a >= c) & (b > c)) == "${{ a != 0 && a >= c && b > c }}"
+
+
+def test_no_bool():
+    a = Expr("a")
+
+    with pytest.raises(TypeError):
+        a and a
+
+    with pytest.raises(TypeError):
+        a or a
