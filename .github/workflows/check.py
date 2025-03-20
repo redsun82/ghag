bf3051af1e74da3224dea36cebdc44bb0b3d08f5
Paolo Tranquilli <redsun82@gihub.com>
2025-03-20 11:09:20 +0100

    Setup uv after checkout

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -4,8 +4,8 @@ from gag.ctx import *
 @workflow
 def check():
     on.pull_request().push()
-    step("Setup uv").uses("astral-sh/setup-uv@v5")
     step("Checkout").uses("actions/checkout@v4")
+    step("Setup uv").uses("astral-sh/setup-uv@v5")
     step("Check formatting").run("uv run black --check .")
     step("Run tests").run("uv run pytest").if_(~cancelled())
     step("Check generation").run("uv run gag --check").if_(~cancelled())
