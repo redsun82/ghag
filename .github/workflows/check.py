a60e2c21ae9737ba9957b9c8985f69a65b01899e
Paolo Tranquilli <redsun82@gihub.com>
2025-03-21 13:28:33 +0100

    Small tweak to `check.py`

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -7,5 +7,5 @@ def check():
     use("actions/checkout@v4")
     use("astral-sh/setup-uv@v5")
     step("Check formatting").run("uv run black --check .")
-    step("Run tests").run("uv run pytest").if_(~cancelled())
-    step("Check generation").run("uv run ghag --check").if_(~cancelled())
+    step("Run tests").if_(~cancelled()).run("uv run pytest")
+    step("Check generation").if_(~cancelled()).run("uv run ghag --check")
